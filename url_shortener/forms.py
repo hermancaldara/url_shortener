954c87bbb9449b55c4e0e659e7f8e0f7edf73396
Herman Caldara <hermancaldara@gmail.com>
2014-12-27 23:03:14 -0200

    Create form to enter a url to be shortened.

---
+++ url_shortener/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+
+class ShortenURLForm(forms.Form):
+    url = forms.URLField(label='URL')
--- url_shortener/tests.py
+++ url_shortener/tests.py
@@ -1,5 +1,8 @@
 from django.core.urlresolvers import resolve
+from django import forms
 from django.test import TestCase, Client
+
+from url_shortener.forms import ShortenURLForm
 
 
 class TestUrl(TestCase):
@@ -17,3 +20,16 @@ class TestView(TestCase):
         response = client.get('/')
 
         self.assertEqual(response.status_code, 200)
+
+
+class TestForm(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.form = ShortenURLForm()
+
+    def test_form_has_a_field_for_an_url(self):
+        self.assertEqual(self.form.base_fields.get('url').label, 'URL')
+
+    def test_field_for_url(self):
+        self.assertIsInstance(self.form.base_fields.get('url'), forms.URLField)

59db9e051f4962112dee3d607821aed1f6f8a09b
Herman Caldara <hermancaldara@gmail.com>
2015-02-19 22:51:20 -0200

    Initial fabfile to deploy the app.

---
+++ fabfile.py
@@ -0,0 +1,53 @@
+from fabric.api import local, cd, run, env, sudo
+
+env.hosts = ['192.168.33.10']
+env.user = 'vagrant'
+env.password = 'vagrant'
+env.colorize_errors = True
+
+PROJECT_NAME = 'url_shortener'
+CODE_DIR = '/var/www/%s/' % PROJECT_NAME
+VIRTUALENV_NAME = PROJECT_NAME
+
+
+def deploy():
+    test()
+    update()
+    requirements()
+    migrate()
+    restart_gunicorn()
+    restart_nginx()
+
+
+def test():
+    local('python manage.py test')
+
+
+def update():
+    with cd(CODE_DIR):
+        run('git pull origin master')
+
+
+def requirements():
+    with cd(CODE_DIR):
+        run('$HOME/.virtualenvs/%s/bin/pip install -r requirements.txt' % VIRTUALENV_NAME)
+
+
+def migrate():
+    with cd(CODE_DIR):
+        run('$HOME/.virtualenvs/%s/bin/python manage.py migrate' % VIRTUALENV_NAME)
+
+
+def restart_gunicorn():
+    with cd(CODE_DIR):
+        run(
+            "ps aux | grep gunicorn | grep -v grep | awk '{ print $2 }' | xargs kill -9 && " +
+            "source $HOME/.virtualenvs/%s/bin/activate &&" % VIRTUALENV_NAME +
+            "gunicorn --config gunicorn.py core.wsgi",
+            pty=False
+        )
+
+
+def restart_nginx():
+    with cd(CODE_DIR):
+        sudo('/etc/init.d/nginx restart')

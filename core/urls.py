4e1b25b2a4c386ca7b89d8ad1b9360d9bfcf8c58
Herman Caldara <hermancaldara@gmail.com>
2015-04-15 22:22:05 -0300

    Active django admin.

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
 
 
 urlpatterns = patterns(
     '',
-    url(r'', include('url_shortener.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('url_shortener.urls')),
 )

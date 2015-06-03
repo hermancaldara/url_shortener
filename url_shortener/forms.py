d6f75b333bbd8b07c793426a23299f1b685dc3d6
Herman Caldara <hermancaldara@gmail.com>
2015-06-02 22:35:26 -0300

    Basic style for the app.

--- url_shortener/forms.py
+++ url_shortener/forms.py
@@ -2,4 +2,7 @@ from django import forms
 
 
 class ShortenURLForm(forms.Form):
-    url = forms.URLField(label='URL')
+    url = forms.URLField(
+        label='URL',
+        widget=forms.TextInput(attrs={'placeholder': 'Put a link to short it'})
+    )
